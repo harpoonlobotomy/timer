69089a48fec3521ee67f2cd7aff81824ac0fde46
Gabriel Lee <harpoonlobotomy@gmail.com>
2025-11-24 14:51:46 +0000

    Update timer_run.py

--- timer_run.py
+++ timer_run.py
@@ -2,24 +2,6 @@ import FreeSimpleGUI as sg
 import time
 from math import floor
 
-def start_clock(minutes: str, sec:str):
-
-    sec = (float(minutes) * 60) + float(sec)
-
-    total_seconds = sec
-
-    while total_seconds > 0:
-        minutes = floor(total_seconds / 60)
-        sec = total_seconds - (minutes * 60)
-        if len(str((round(sec)))) < 2:
-            sec = "0" + str(round(sec))
-            print(f" [[   {minutes}:{sec}]]")
-        else:
-            print(f" [[   {minutes}:{round(sec)}]]")
-
-        time.sleep(1)  # every second
-        total_seconds -= 1  # take one second
-
 def counter_func(total_seconds):
     minutes = (floor(total_seconds / 60))
     sec = total_seconds - (minutes * 60)
@@ -30,13 +12,6 @@ def counter_func(total_seconds):
     print(f" [[   {minutes}:{sec}]]")
 
     return str(minutes), str(sec)
-
-def clock_update(total_seconds):
-
-    time.sleep(1)  # every second
-    total_seconds -= 1  # take one second
-
-    return total_seconds
 
 
 def parse_inputtext(input_text:str):
@@ -60,10 +35,26 @@ def parse_inputtext(input_text:str):
 
     return minute, sec
 
-def counter_text(key_str="minutes"):
+
+def pause_toggle(paused): # paused is a bool, represents existing state, not desired state.
+
+
+
+
+background_colour = "darkblue"
+numbers_colour="yellow"
+paused_numbers = background_colour
+
+def counter_text(key_str="minutes", txt_col=numbers_colour):
     counting_font='Courier 40 bold'
 
-    return sg.Text(text='00', key=key_str, size=(3,1), background_color="blue", relief="sunken", border_width=4, justification="center", font=counting_font)
+    return sg.Text(text='00', key=key_str, size=(3,1), background_color=background_colour, relief="sunken", border_width=4, justification="center", font=counting_font, text_color=txt_col)
+
+def make_button(width:float=10, height:float=10, key_str:str="Pause"):
+        key_upper = key_str.upper()
+        key_formatting = str("-" + key_upper + '-')
+        print(key_formatting)
+        return sg.Button(key_str, key=key_formatting, size=(10,10))
 
 def make_window():
     sg.theme('DarkAmber')  # Add a touch of color
@@ -79,8 +70,12 @@ def make_window():
     column_two = [[sg.InputText(default_text="5:18", size=(16, 20), border_width=2, focus=False, enable_events=True, # focus=True, #turning this off so I can wipe the text when  you click it.
                                 key='-INPUT-', tooltip="Input styles: \n     min.sec (15.10), \n     min:sec (15:10), \n     15m10s, \n     15m, \n     10s, \n     15 (defaults to minutes if not specified.)")
                    ],
-                  [sg.Stretch(), sg.Button('Start', key='-START-'), sg.Button('Exit', key='-EXIT-'), sg.Stretch()],
+                  [#sg.Stretch(),
+                    make_button(width=5, height=5, key_str="Start"), make_button(width=5, height=5, key_str="Pause"), make_button(width=5, height=5, key_str="Exit")
+                      #sg.Button('Start', key='-START-', size=(30,15)), sg.Button('Pause', key='-PAUSE-', size=(20,10)), ], [sg.Button('Exit', key='-EXIT-', size=(20,10)), sg.Stretch()],
                   ]
+    ]
+
 
     main_contents = [
         [sg.Column(layout=column_one), sg.Column(layout=column_two)],
@@ -91,46 +86,76 @@ def make_window():
 
     # Create the Window
     minute = sec = 00
+    running=False
+    paused=False
+    total_sec=10
     window = sg.Window('Window Title', layout, keep_on_top=True, finalize=True)
     window['-INPUT-'].bind("<Return>", "_Enter")
     while True:
         event, values = window.read(timeout=1000)
         if event == "-START-" or event == "-INPUT-" + "_Enter":
+            if paused:
+                pause_toggle(paused)
+                paused=False
             input_text: str = values['-INPUT-']
             if input_text:
                 print('You entered ', input_text)
                 minute, sec = parse_inputtext(input_text)
                 print(f"Minute: {minute}, sec: {sec}")
-                total_sec = (float(minute) * 60) + float(sec)
                 print("Starting clock...")
-                while True:
-                    minutes, seconds = counter_func(total_sec)
-                    window['minutes'].update(minutes)
-                    window['seconds'].update(seconds)
-                    print(f"min: {minutes}, sec: {seconds}")
-                    total_sec = clock_update(total_sec)
-                    window.finalize()
-                    if total_sec < 0:
-                        break
+                total_sec = (float(minute) * 60) + float(sec)
+
+                window['minutes'].update(minute)
+                window['seconds'].update(sec)
+                running=True
+                window['-START-'].update("[Restart]")
                 # I really want to animate it opening its eyes when the timer's about to start.
             else:
                 print("Please enter a number")
 
-        if event == "Start":
-            running = True
+        if event == sg.WIN_CLOSED or event == '-EXIT-':  # if user closes window or clicks cancel
+            running=False
+            break
 
-        if event == "Stop":
+        if total_sec < 0:
             running = False
+
+        if event == "-PAUSE-":
+            if running:
+                running = False
+                window['-PAUSE-'].update("[Paused]")
+                window['minutes'].update(text_color=paused_numbers)
+                window['seconds'].update(text_color=paused_numbers)
+                tog_on=False
+                paused=True
+
+            else:
+                running = True
+                window['-PAUSE-'].update("Pause")
+                window['minutes'].update(text_color=numbers_colour)
+                window['seconds'].update(text_color=numbers_colour)
+                paused=False
+
+        if paused:
+            if tog_on:
+                window['minutes'].update(text_color=paused_numbers)
+                window['seconds'].update(text_color=paused_numbers)
+                tog_on=False
+            else:
+                window['minutes'].update(text_color=numbers_colour)
+                window['seconds'].update(text_color=numbers_colour)
+                tog_on=True
+            #window['seconds'].visible=False
 
         if running:
-            total_seconds -= 1
-            window["-TIMER-"].update(total_seconds)
-
-        if total_seconds <= 0:
-            running = False
-
-        if event == sg.WIN_CLOSED or event == '-EXIT-':  # if user closes window or clicks cancel
-            break
+            minutes, seconds = counter_func(total_sec)
+            window['minutes'].update(minutes)
+            window['seconds'].update(seconds)
+            print(f"min: {minutes}, sec: {seconds}")
+            total_sec -= 1
+            window.finalize()
+            if total_sec < 0:
+                running=False
 
     window.close()
 
